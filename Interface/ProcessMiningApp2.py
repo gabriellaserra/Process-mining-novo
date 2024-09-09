b13b74be173f7c9a81e4a7273ffd4b63fe42a081
@gabriellaserra <gabriellaserra@tecgraf.puc-rio.br>
2024-09-09 08:07:41 -0300

    duplicando o arquivo para converter pra jupyter notebook

---
+++ Interface/ProcessMiningApp2.py
@@ -0,0 +1,20 @@
+
+# pip install pm4py
+# pip install pandas
+# pip install customtkinter
+# pip install CTkMessagebox
+
+from tkinter import *
+import tkinter as tk
+import customtkinter as s
+
+from Janela.Janela import *
+
+raiz = s.CTk(className='Process Mining GUI')
+raiz.title('Process Mining GUI')
+s.set_appearance_mode("Dark")
+s.set_default_color_theme("dark-blue")
+raiz.geometry('960x540')
+Janela(raiz)
+raiz.mainloop()
+# teste
